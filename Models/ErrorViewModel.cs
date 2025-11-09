2b0f1eba8281f7f909c05866375a1896551e8c50
Jayden <jayden.pearce12@gmail.com>
2025-11-09 09:24:52 +0200

    Initial commit - Arksoft tech test project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Arksoft_tech_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
